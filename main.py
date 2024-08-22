b50d09834bbcf5520f1c6e59221c5cce380a797f
Hermes J. Pappas <hjp@hjp.gr>
2024-08-22 14:02:40 +0300

    Give a more comprehensive and better formated report

--- main.py
+++ main.py
@@ -5,8 +5,24 @@ def main():
     num_words = get_num_words(text)
     letters_dict = get_characters_count(text)
 
-    
+    chars_dict_list = []
+    for char in letters_dict:
+        if char.isalpha():
+            chars_dict_list.append({"name": char, "num": letters_dict[char]})
 
+    chars_dict_list.sort(reverse=True, key=sort_on)
+
+    print ("--- Begin report of books/frankenstein.txt ---")
+    print(f"{num_words} words found in the document")
+    print()
+    for dict in chars_dict_list:
+        print(f"The '{dict['name']}' character was found {dict['num']} times")
+
+    print()
+    print("--- End report ---")
+
+def sort_on(dict):
+    return dict["num"]
 
 
 def get_characters_count(text):
