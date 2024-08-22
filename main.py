642e95a62d94bc617567791faad8264e86acfd80
Hermes J. Pappas <hjp@hjp.gr>
2024-08-22 13:55:15 +0300

    Count characters

--- main.py
+++ main.py
@@ -1,8 +1,26 @@
 def main():
     book_path = "books/frankenstein.txt"
     text = get_book_text(book_path)
+
     num_words = get_num_words(text)
-    print(f"{num_words} words found in the document")
+    letters_dict = get_characters_count(text)
+
+    
+
+
+
+def get_characters_count(text):
+    words = text.split()
+    letters_dict = {}
+    for word in words:
+        lowercase_word = word.lower()
+        for character in lowercase_word:
+            if character in letters_dict:
+                letters_dict[character] += 1
+            else:
+                letters_dict[character] = 1
+
+    return letters_dict
 
 
 def get_book_text(path):
